c5602c3ff507ca55dcf5cd68422a9e39b117bcc0
akshit <akshitkandpal74242@gmail.com>
2024-06-29 14:18:52 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
         />
 
         <h2>Akshit Kandpal</h2>
-        <p>©2023 Made with ❤️ by Akshit.</p>
+        <p>©2023 Made by Akshit.</p>
       </div>
 
       <aside>
